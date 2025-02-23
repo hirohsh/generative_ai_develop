4f512ee698a95c24cfab942083580e289fc037a1
hhiro <waon.jpkbk@gmail.com>
2025-02-23 10:23:46 +0900

    #2 converseAPI対応

--- app/backend/fastapi_backend/app/routers/v1/bedrock_router.py
+++ app/backend/fastapi_backend/app/routers/v1/bedrock_router.py
@@ -1,25 +1,43 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Annotated
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Body, HTTPException
 from fastapi.responses import ORJSONResponse
 
-from app.services.bedrock import MODEL_SERVICE_DEPENDS, USER_INPUT_QUERY
+from app.schemas.bedrock_schema import MessageList
 
 if TYPE_CHECKING:
-    from mypy_boto3_bedrock_runtime.type_defs import BlobTypeDef
+    from collections.abc import Sequence
 
-from app.interfaces.bedrock_interface import BedrockModelBase, InvokeRequestTypeDef, ISupportsInvokeModel
+    from mypy_boto3_bedrock_runtime.type_defs import BlobTypeDef, MessageUnionTypeDef
+
+from app.dependencies.bedrock_dependencies import MODEL_SERVICE_DEPENDS
+from app.interfaces.bedrock_interface import BedrockModelBase, ISupportsConverse, ISupportsInvokeModel
 
 router = APIRouter(prefix="/bedrock", tags=["Bedrock"])
 
 
+@router.post("/converse")
+def converse(
+    user_input: Annotated[MessageList, Body(..., description="ConverseAPI用のユーザー入力", embed=True)],
+    bedrock_service: Annotated[BedrockModelBase, MODEL_SERVICE_DEPENDS],
+) -> ORJSONResponse:
+    if not isinstance(bedrock_service, ISupportsConverse):
+        raise HTTPException(status_code=400, detail="このモデルは対応してません")
+
+    converse_messages: Sequence[MessageUnionTypeDef] = bedrock_service.generate_converse_messages(user_input)
+    reply_text: str = bedrock_service.converse(converse_messages)
+    return ORJSONResponse(content=reply_text)
+
+
 @router.post("/invoke-model")
 def invoke_model(
-    user_input: InvokeRequestTypeDef = USER_INPUT_QUERY, bedrock_service: BedrockModelBase = MODEL_SERVICE_DEPENDS
+    user_input: Annotated[MessageList, Body(..., description="ユーザーの入力", embed=True)],
+    bedrock_service: Annotated[BedrockModelBase, MODEL_SERVICE_DEPENDS],
 ) -> ORJSONResponse:
     if not isinstance(bedrock_service, ISupportsInvokeModel):
         raise HTTPException(status_code=400, detail="このモデルは対応してません")
 
     payload: BlobTypeDef = bedrock_service.generate_invoke_model_payload(user_input)
 
-    return bedrock_service.invoke_model(payload)
+    response: str = bedrock_service.invoke_model(payload)
+    return ORJSONResponse(content=response)
---
+++ app/backend/fastapi_backend/app/types/bedrock_type_defs.py
@@ -0,0 +1,68 @@
+from enum import Enum
+from typing import Generic, NotRequired, TypedDict, TypeVar
+
+from fastapi import UploadFile
+from mypy_boto3_bedrock_runtime.type_defs import (
+    ConverseRequestRequestTypeDef,
+    ConverseStreamRequestRequestTypeDef,
+    InvokeModelRequestRequestTypeDef,
+    InvokeModelWithResponseStreamRequestRequestTypeDef,
+)
+
+###############################################################
+# 共通
+###############################################################
+
+T = TypeVar("T")
+
+InvokeRequestTypeDef = str | bytes | UploadFile  # invoke_modelのリクエストで受け取るパラメーターの型
+
+
+class ModelType(str, Enum):
+    """モデルの種類を表す列挙型"""
+
+    LLAMA3 = "Llama3"
+
+
+class SdkConfigTypeDef(TypedDict):
+    """
+    Bedrockランタイムクライアントの各メソッドで使用する設定の型定義
+    """
+
+    invoke: NotRequired[InvokeModelRequestRequestTypeDef]  # invoke_model
+    invoke_stream: NotRequired[InvokeModelWithResponseStreamRequestRequestTypeDef]  # invoke_model_with_response_stream
+    converse: NotRequired[ConverseRequestRequestTypeDef]  # converse
+    converse_stream: NotRequired[ConverseStreamRequestRequestTypeDef]  # converse_stream
+
+
+class ConfigTypeDef(TypedDict, Generic[T]):
+    """
+    モデルサービスで使用する設定の型定義
+    """
+
+    sdk: SdkConfigTypeDef
+    model: NotRequired[T]
+
+
+###############################################################
+# Llama3
+###############################################################
+
+
+class LlamaInvokeRequestModelConfigTypeDef(TypedDict):
+    """
+    Llama3のinvoke_modelに使用するパラメーター型定義
+    """
+
+    prompt: str
+    temperature: float
+    top_p: float
+    max_gen_len: int
+
+
+class LlamaConfigTypeDef(TypedDict):
+    """
+    Llama3サービス 各種メソッドのモデル自体に渡すパラメーター型定義
+    """
+
+    invoke: LlamaInvokeRequestModelConfigTypeDef
